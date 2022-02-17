10705dbdaf9462e5045f742ea05319f927d9e9ba
Sandeep Dawre <sandy.dawre123@gmail.com>
2022-02-17 09:57:57 +0530

    Refactored controller api for swagger and added missing test for controller

--- src/main/java/com/hexad/librarymanagment/controller/LibraryController.java
+++ src/main/java/com/hexad/librarymanagment/controller/LibraryController.java
@@ -4,6 +4,7 @@ import com.hexad.librarymanagment.exception.BookNotFoundException;
 import com.hexad.librarymanagment.model.Book;
 import com.hexad.librarymanagment.model.User;
 import com.hexad.librarymanagment.service.LibraryService;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -16,17 +17,30 @@ public class LibraryController {
     @Autowired
     private LibraryService libraryService;
 
+    @ApiOperation(value = "Provide a book id  ",
+            notes = "Give book details in response",
+            response = User.class
+    )
     @GetMapping(path = "/books/{bookId}")
     public Book getBook(@PathVariable int bookId) {
         return libraryService.getBook(bookId);
     }
 
+
+    @ApiOperation(value = "Save a book ",
+            notes = "Displays a book saved in library database",
+            response = User.class
+    )
     @PutMapping(path = "/books/savebook")
     public Book saveBook(@RequestBody Book book) {
         return libraryService.saveBook(book);
     }
 
 
+    @ApiOperation(value = "Give user details to be saved in library database",
+            notes = "Displays saved user in library",
+            response = User.class
+    )
     @PutMapping(path = "/saveuser")
     public User saveUser(@RequestBody User user) {
         return libraryService.saveUser(user);
@@ -37,7 +51,10 @@ public class LibraryController {
         return libraryService.getAllBooks();
     }
 
-
+    @ApiOperation(value = "Provide a user id ",
+            notes = "Show you user details of the user",
+            response = User.class
+    )
     @GetMapping(path = "/users/{userId}")
     public User getUser(@PathVariable Integer userId) {
         return libraryService.getUserById(userId);
--- src/test/java/com/hexad/librarymanagment/controller/LibraryControllerTest.java
+++ src/test/java/com/hexad/librarymanagment/controller/LibraryControllerTest.java
@@ -2,7 +2,9 @@ package com.hexad.librarymanagment.controller;
 
 import com.hexad.librarymanagment.exception.BookNotFoundException;
 import com.hexad.librarymanagment.model.Book;
+import com.hexad.librarymanagment.model.User;
 import com.hexad.librarymanagment.service.LibraryService;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,7 +15,9 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.BDDMockito.given;
@@ -25,14 +29,22 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @ContextConfiguration(classes = LibraryController.class)
 public class LibraryControllerTest {
 
+    private static final Integer BOOK_ID = 1;
+    private final Integer USER_ID = 1;
     @Autowired
     private MockMvc mockMvc;
     @MockBean
     private LibraryService libraryService;
+    private List<Book> books;
+
+    @Before
+    public void setup() {
+        books = new ArrayList<>();
+    }
 
     @Test
     public void getBook_shouldReturnBook() throws Exception {
-        given(libraryService.getBook(anyInt())).willReturn(new Book(100, "Test book name ","Test author name", "publisher1",1));
+        given(libraryService.getBook(anyInt())).willReturn(new Book(100, "Test book name ", "Test author name", "publisher1", 1));
         mockMvc.perform(MockMvcRequestBuilders.get("/library/books/100")).
                 andExpect(status().isOk()).
                 andExpect(jsonPath("name").value("Test book name ")).
@@ -48,11 +60,29 @@ public class LibraryControllerTest {
 
     @Test
     public void getAllBooks_shouldReturnListOfBooks() throws Exception {
-        given(libraryService.getAllBooks()).willReturn(Arrays.asList(new Book(100,"Test book Name ","Test author name","publisher",1),
-                new Book(200,"book name2","Test auther name","some publisher",1)));
+        given(libraryService.getAllBooks()).willReturn(Arrays.asList(new Book(100, "Test book Name ", "Test author name", "publisher", 1),
+                new Book(200, "book name2", "Test auther name", "some publisher", 1)));
         mockMvc.perform(MockMvcRequestBuilders.get("/library/books/")).
                 andExpect(jsonPath("$[0].name").value("Test book Name ")).
                 andExpect(jsonPath("$[1].publisher").value("some publisher"));
     }
 
+    @Test
+    public void getUser_shouldReturnUser() throws Exception {
+        given(libraryService.getUserById(USER_ID)).willReturn(new User(USER_ID, "test name", books));
+        mockMvc.perform(MockMvcRequestBuilders.get("/library/users/" + USER_ID)).
+                andExpect(status().isOk()).
+                andExpect(jsonPath("name").value("test name"));
+    }
+
+    @Test
+    public void getBook_shouldThrowUserNotFoundException() throws Exception {
+        given(libraryService.getBook(BOOK_ID)).willReturn(new Book(BOOK_ID,"book name","Author name","Publisher name",10));
+        mockMvc.perform(MockMvcRequestBuilders.get("/library/books/" + BOOK_ID)).
+                andExpect(status().isOk()).
+                andExpect(jsonPath("name").value("book name")).
+                andExpect(jsonPath("author").value("Author name")).
+                andExpect(jsonPath("noOfCopies").value(10));
+    }
 }
+
