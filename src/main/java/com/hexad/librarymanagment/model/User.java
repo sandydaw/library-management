b059a5ec115a1259d164ac6a3b25801d902831f2
Sandeep Dawre <sandy.dawre123@gmail.com>
2025-06-30 22:56:48 +0530

    Fet2 (#30)
    * added bulder annotation
    
    * changed datatype and added adress
    
    * Updated add name and removed constructor
    
    * Updated addres again
    
    * Addres list and user id changed
    
    * Changed userid and addrss name
    
    * Update name again

--- src/main/java/com/hexad/librarymanagment/model/User.java
+++ src/main/java/com/hexad/librarymanagment/model/User.java
@@ -19,11 +19,13 @@ import java.util.List;
 @Builder
 public class User {
     @Id
-    private String  userid;
+    private int  userId;
     private String name;
     @OneToMany
     @Cascade({CascadeType.ALL})
     @ApiModelProperty(hidden = true)
     private List<Book> borrowBookList;
-    private List<Address> addresses;
+
+    private List<Address> addressesList;
+
 }
